5f5e837592a71fb61dd2899efdde135bd0672f9e
Novruz <novruzismylovv@gmail.com>
2026-01-06 22:58:15 +0100

    Initial commit

---
+++ first-project-java-spring1/src/main/java/pl/edu/vistula/first_project_java_spring1/FirstProjectJavaSpring1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpring1Application.class, args);
+	}
+
+}
---
+++ first-project-java-spring1/src/test/java/pl/edu/vistula/first_project_java_spring1/FirstProjectJavaSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
